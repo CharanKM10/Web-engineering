8fef01e02bf85a5d55a9b89682e8d7b50bf46d32
CharanKM10 <103021499+CharanKM10@users.noreply.github.com>
2022-07-15 11:32:37 +0530

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,7 @@
+function firstcookie() 
+{
+	let fullname = document.getElementsById('fullname').value;
+	let email = document.getElementsById('email').value;
+	let pass = document.getElementsById('pass').value;
+	let repass = document.getElementsById('repass').value;
+}
